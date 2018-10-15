f264d6397b96bf5e4fedc62c9af09a4197267a33
G-regory <carriel.g.ca@gmail.com>
2018-10-15 19:04:30 +0200

    Control modifier

--- coursera/Coursera2/Velo.java
+++ coursera/Coursera2/Velo.java
@@ -1,3 +1,5 @@
+package test;
+
 import java.util.Scanner;
 
 public class Velo {
@@ -15,7 +17,7 @@ public class Velo {
 
 		/********** Début initialisation des variables ***************/
 
-		int horaireDebut = 0, horaireFin = 0, tarifHoraireNuit = 0, tarifHoraireJour = 0, totalHeureLocation = 0,
+		int tarifHoraireNuit = 0, tarifHoraireJour = 0, totalHeureLocation = 0,
 				totalHeureJour = 0, totalHeureNuit = 0;
 		double tarif;
 		String groupe1 = "", groupe2 = "", groupe3 = "", groupeA = "", groupeB = "", groupeC = "";
@@ -39,138 +41,128 @@ public class Velo {
 
 		/*********** Contrôle **************/
 		if (controleOk) {
-			horaireDebut = debut;
-			horaireFin = fin;
-		}
-		/*********** Contrôle **************/
-
-//******Début classification de l'heure de Début**********************
-
-		if (horaireDebut < 7) {
-			if ((horaireDebut == debut) || (horaireDebut > 0)) {
-				groupeA = "a";
+			
+			//******Début classification de l'heure de Début**********************
+			if (debut < 7) {
+					groupeA = "a";
+			} else if (debut < 17) {
+				groupeB = "b";
+			} else if (debut >= 17) {
+				groupeC = "c";
 			}
-		} else if (horaireDebut < 17) {
-			groupeB = "b";
-		} else if (horaireDebut >= 17) {
-			groupeC = "c";
-		}
+			//******Fin classification de l'heure de Début**********************
+			
+			//******Début classification de l'heure de Fin*****************
 
-//******Fin classification de l'heure de Début**********************
-
-//******Début classification de l'heure de Fin*****************
-
-		if (horaireFin < 7) {
-			if (horaireFin < 7) {
-				if ((horaireFin == fin) || (horaireFin > 0)) {
-					groupe1 = "a";
-				}
-			} else if (horaireFin < 17) {
-				if (groupeA == "a") {
-					groupe2 = "a";
-				} else if (groupeB == "b") {
-					groupe2 = "b";
-				}
-			} else if (horaireFin >= 17) {
-				if (groupeA == "a") {
-					groupe3 = "a";
-				} else if (groupeB == "b") {
-					groupe3 = "b";
-				} else if (groupeC == "c") {
-					groupe3 = "c";
+			if (fin < 7) {
+				if (fin < 7) {
+						groupe1 = "a";
+				} else if (fin < 17) {
+					if (groupeA == "a") {
+						groupe2 = "a";
+					} else if (groupeB == "b") {
+						groupe2 = "b";
+					}
+				} else if (fin >= 17) {
+					if (groupeA == "a") {
+						groupe3 = "a";
+					} else if (groupeB == "b") {
+						groupe3 = "b";
+					} else if (groupeC == "c") {
+						groupe3 = "c";
+					}
 				}
 			}
-		}
 
-//******Fin classification de l'heure de Fin*****************
+	//******Fin classification de l'heure de Fin*****************
+			
+			//**********Début calcule horaire groupeA************************
 
-//**********Début calcule horaire groupeA************************
-
-		if ((groupeA == "a") && (groupe1 == "a")) {
-			totalHeureLocation = horaireFin - horaireDebut;
-			totalHeureNuit = totalHeureLocation;
-			tarifHoraireJour = totalHeureJour;
-			tarifHoraireNuit = totalHeureNuit;
-			System.out.println("Vous avez loué votre vélo pendant");
-			System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
-			tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
-			System.out.print("Le montant total à  payer est de ");
-			System.out.print(tarif);
-			System.out.println(" franc(s).");
-		} else if ((groupeA == "a") && (groupe2 == "a")) {
-			totalHeureJour = horaireFin - 7;
-			totalHeureLocation = horaireFin - horaireDebut;
-			totalHeureNuit = totalHeureLocation - totalHeureJour;
-			tarifHoraireJour = totalHeureJour;
-			tarifHoraireNuit = totalHeureNuit;
-			System.out.println("Vous avez loué votre vélo pendant");
-			System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
-			if (totalHeureJour > 0) {
+			if ((groupeA == "a") && (groupe1 == "a")) {
+				totalHeureLocation = fin - debut;
+				totalHeureNuit = totalHeureLocation;
+				tarifHoraireJour = totalHeureJour;
+				tarifHoraireNuit = totalHeureNuit;
+				System.out.println("Vous avez loué votre vélo pendant");
+				System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
+				tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
+				System.out.print("Le montant total à  payer est de ");
+				System.out.print(tarif);
+				System.out.println(" franc(s).");
+			} else if ((groupeA == "a") && (groupe2 == "a")) {
+				totalHeureJour = fin - 7;
+				totalHeureLocation = fin - debut;
+				totalHeureNuit = totalHeureLocation - totalHeureJour;
+				tarifHoraireJour = totalHeureJour;
+				tarifHoraireNuit = totalHeureNuit;
+				System.out.println("Vous avez loué votre vélo pendant");
+				System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
+				if (totalHeureJour > 0) {
+					System.out.println(totalHeureJour + " heure(s) au tarif horaire de 2.0 franc(s)");
+				}
+				tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
+				System.out.print("Le montant total à  payer est de ");
+				System.out.print(tarif);
+				System.out.println(" franc(s).");
+			} else if ((groupeA == "a") && (groupe3 == "a")) {
+				totalHeureLocation = fin - debut;
+				totalHeureJour = 10;
+				totalHeureNuit = totalHeureLocation - totalHeureJour;
+				tarifHoraireJour = totalHeureJour;
+				tarifHoraireNuit = totalHeureNuit;
+				System.out.println("Vous avez loué votre vélo pendant");
+				System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
 				System.out.println(totalHeureJour + " heure(s) au tarif horaire de 2.0 franc(s)");
+				tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
+				System.out.print("Le montant total à  payer est de ");
+				System.out.print(tarif);
+				System.out.println(" franc(s).");
 			}
-			tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
-			System.out.print("Le montant total à  payer est de ");
-			System.out.print(tarif);
-			System.out.println(" franc(s).");
-		} else if ((groupeA == "a") && (groupe3 == "a")) {
-			totalHeureLocation = horaireFin - horaireDebut;
-			totalHeureJour = 10;
-			totalHeureNuit = totalHeureLocation - totalHeureJour;
-			tarifHoraireJour = totalHeureJour;
-			tarifHoraireNuit = totalHeureNuit;
-			System.out.println("Vous avez loué votre vélo pendant");
-			System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
-			System.out.println(totalHeureJour + " heure(s) au tarif horaire de 2.0 franc(s)");
-			tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
-			System.out.print("Le montant total à  payer est de ");
-			System.out.print(tarif);
-			System.out.println(" franc(s).");
+//		********Fin calcule horaire groupeA***************
+
+			// **********Début calcule horaire groupeB************************
+
+			if ((groupeB == "b") && (groupe2 == "b")) {
+				totalHeureLocation = fin - debut;
+				totalHeureJour = totalHeureLocation;
+				System.out.println("Vous avez loué votre vélo pendant");
+				System.out.println(totalHeureJour + " heure(s) au tarif horaire de 2.0 franc(s)");
+				tarifHoraireJour = totalHeureJour;
+				tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
+				System.out.print("Le montant total à  payer est de ");
+				System.out.print(tarif);
+				System.out.println(" franc(s).");
+			} else if ((groupeB == "b") && (groupe3 == "b")) {
+				totalHeureJour = 17 - debut;
+				totalHeureLocation = fin - debut;
+				totalHeureNuit = totalHeureLocation - totalHeureJour;
+				tarifHoraireJour = totalHeureJour;
+				tarifHoraireNuit = totalHeureNuit;
+				System.out.println("Vous avez loué votre vélo pendant");
+				System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
+				System.out.println(totalHeureJour + " heure(s) au tarif horaire de 2.0 franc(s)");
+				tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
+				System.out.print("Le montant total à  payer est de ");
+				System.out.print(tarif);
+				System.out.println(" franc(s).");
+			}
+			// **********Fin calcule horaire groupeB************************
+
+			// **********Début calcule horaire groupeC************************
+
+			if ((groupeC == "c") && (groupe3 == "c")) {
+				totalHeureLocation = fin - debut;
+				totalHeureNuit = totalHeureLocation;
+				tarifHoraireNuit = totalHeureNuit;
+				System.out.println("Vous avez loué votre vélo pendant");
+				System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
+				tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
+				System.out.print("Le montant total à  payer est de ");
+				System.out.print(tarif);
+				System.out.println(" franc(s).");
+			}
+			// **********Fin calcule horaire groupeC************************			
 		}
-//	********Fin calcule horaire groupeA***************
-
-		// **********Début calcule horaire groupeB************************
-
-		if ((groupeB == "b") && (groupe2 == "b")) {
-			totalHeureLocation = horaireFin - horaireDebut;
-			totalHeureJour = totalHeureLocation;
-			System.out.println("Vous avez loué votre vélo pendant");
-			System.out.println(totalHeureJour + " heure(s) au tarif horaire de 2.0 franc(s)");
-			tarifHoraireJour = totalHeureJour;
-			tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
-			System.out.print("Le montant total à  payer est de ");
-			System.out.print(tarif);
-			System.out.println(" franc(s).");
-		} else if ((groupeB == "b") && (groupe3 == "b")) {
-			totalHeureJour = 17 - horaireDebut;
-			totalHeureLocation = horaireFin - horaireDebut;
-			totalHeureNuit = totalHeureLocation - totalHeureJour;
-			tarifHoraireJour = totalHeureJour;
-			tarifHoraireNuit = totalHeureNuit;
-			System.out.println("Vous avez loué votre vélo pendant");
-			System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
-			System.out.println(totalHeureJour + " heure(s) au tarif horaire de 2.0 franc(s)");
-			tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
-			System.out.print("Le montant total à  payer est de ");
-			System.out.print(tarif);
-			System.out.println(" franc(s).");
-		}
-		// **********Fin calcule horaire groupeB************************
-
-		// **********Début calcule horaire groupeC************************
-
-		if ((groupeC == "c") && (groupe3 == "c")) {
-			totalHeureLocation = horaireFin - horaireDebut;
-			totalHeureNuit = totalHeureLocation;
-			tarifHoraireNuit = totalHeureNuit;
-			System.out.println("Vous avez loué votre vélo pendant");
-			System.out.println(totalHeureNuit + " heure(s) au tarif horaire de 1.0 franc(s)");
-			tarif = (tarifHoraireNuit * 1) + (tarifHoraireJour * 2);
-			System.out.print("Le montant total à  payer est de ");
-			System.out.print(tarif);
-			System.out.println(" franc(s).");
-		}
-		// **********Fin calcule horaire groupeC************************
-
 		/*******************************************
 		 * Ne rien modifier apres cette ligne.
 		 *******************************************/
